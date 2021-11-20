ca2c07dbf5be3a742286069303b93c36a72e08bb
Sayan Mondal <sayanmondal2098@gmail.com>
2021-11-20 23:04:04 +0530

    update basic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: ' https://sayanmondal2098.github.io/',
+  baseUrl: '/PatchDocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
